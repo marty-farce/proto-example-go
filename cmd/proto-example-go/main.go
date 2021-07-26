82f8346343ec6d265f89e20ae058fa7a91b3d58c
marty-farce <marty.farce@protonmail.com>
2021-07-25 23:45:37 -0700

    final

--- cmd/proto-example-go/main.go
+++ cmd/proto-example-go/main.go
@@ -5,6 +5,6 @@ import (
 )
 
 func main() {
-
 	fmt.Println(encode())
+	fmt.Println(decode(encode()))
 }
--- cmd/proto-example-go/proto.go
+++ cmd/proto-example-go/proto.go
@@ -1,10 +1,42 @@
 package main
 
-func encode() []byte {
+import (
+	"encoding/json"
+	"log"
 
-	return []byte("[2, 3]")
+	"google.golang.org/protobuf/proto"
+)
+
+func encode() []byte {
+	var hello int32
+	hello = 22
+	isMarried := false
+
+	p := Person{
+		Name:          "Buford",
+		Age: &hello,
+		IsMarried: &isMarried,
+	}
+
+	out, err := proto.Marshal(&p)
+	if err != nil {
+		log.Fatalln("failed to encode: ", err)
+	}
+
+	return out
 }
 
-func decode(protobuf []byte) {
+func decode(protobuf []byte) string {
 
+	p := Person{}
+	if err := proto.Unmarshal(protobuf, &p); err != nil {
+		log.Fatalln("failed to parse", err)
+	}
+	
+	out, err := json.Marshal(p)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(out)
 }
